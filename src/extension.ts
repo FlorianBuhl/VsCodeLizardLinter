369e62844a7637681e1ac2a5f7b0c792b59725a5
Florian Buhl <florian.buhl@freenet.de>
2022-09-05 22:41:04 +0200

    Extracted lizard related function in extra module

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as lizard from './lizard';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -24,97 +25,28 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 
 	// execute lint command
-	disposable = vscode.commands.registerCommand('lizard-linter.lint', lint);
+	disposable = vscode.commands.registerCommand('lizard-linter.lint', lizard.lint);
 	context.subscriptions.push(disposable);
 
+	context.subscriptions.push(lizard.createDiagnosticCollection());
+
 	// add filesystemWatchers to be informed about new cyclomatic complexity logs
-	if(undefined !== vscode.workspace.workspaceFolders){
-		let filesystemWatcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(vscode.workspace.workspaceFolders[0], `log/*.log`));
-		filesystemWatcher.onDidChange((uri) => {
-			analyzeLizardLogFiles([uri]);
-		});
-		filesystemWatcher.onDidCreate((uri) => {
-			analyzeLizardLogFiles([uri]);
-		});
-	}
+	// if(undefined !== vscode.workspace.workspaceFolders){
+	// 	let filesystemWatcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(vscode.workspace.workspaceFolders[0], `log/*.log`), true, false, true);
+	// 	filesystemWatcher.onDidChange((uri) => {
+	// 		lizard.analyzeLizardLogFiles([uri]);
+	// 	});
+	// 	filesystemWatcher.onDidCreate((uri) => {
+	// 		onDidCreate([uri]);
+	// 	});
+	// }
+
+	disposable = vscode.tasks.onDidEndTask(lizard.onDidEndTask);
+	context.subscriptions.push(disposable);
 }
 
-export function lint()
-{
-	let message;
-
-	if((undefined !== vscode.window.activeTextEditor)
-	&& (undefined !== vscode.workspace.workspaceFolders)) {
-		const openedFilePath = vscode.window.activeTextEditor.document.uri.fsPath;
-		const openedFileName = path.parse(openedFilePath).name;
-		const logFolderUri = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, "log");
-		const logFileUri = vscode.Uri.joinPath(logFolderUri, openedFileName + ".log");
-
-		console.log(`openedFilePath ${openedFilePath}\nopenedFileName ${openedFileName}\nlogFolderUri ${logFolderUri.fsPath}\nlogFileUri ${logFileUri.fsPath}`);
-
-		// create lizard log folder if it does not exist
-		if(!fs.existsSync(logFolderUri.fsPath)) {
-			fs.mkdirSync(logFolderUri.fsPath);
-		}
-		// remove prevously generated log file
-		if(fs.existsSync(logFileUri.fsPath)){
-			fs.rmSync(logFileUri.fsPath);
-		}
-
-		const terminal = vscode.window.createTerminal("lizard_linter_terminal");
-		terminal.sendText(`lizard ${openedFilePath} >> ${logFileUri.fsPath}`);
-	}
-
-	vscode.window.showInformationMessage('Execute lint ' + message);
-}
-
-export function analyzeLizardLogFiles(lizardLogFilesUri: vscode.Uri[]){
-	let lizardDiagCol: Map<vscode.Uri, vscode.Diagnostic[] | undefined>;
-
-	for (let lizardLogFileUri of lizardLogFilesUri) {
-		vscode.workspace.openTextDocument(lizardLogFileUri).then((document) => {
-			let text = document.getText();
-			console.log(text);
-			lizardDiagCol = analyzeLizardLogFile(text);
-
-			// for (let lintDiagColKey of lizardDiagCol.keys()) {
-			// 	diagnosticCollection.set(lintDiagColKey, lizardDiagCol.get(lintDiagColKey));
-			// }
-		});
-	}
-}
-
-export function analyzeLizardLogFile(text: string): Map<vscode.Uri, vscode.Diagnostic[] | undefined>{
-
-	let diagnosticCollection = new Map<vscode.Uri, vscode.Diagnostic[] | undefined>();
-	let diagnostics: vscode.Diagnostic[] | undefined = undefined;
-	let uri: vscode.Uri | undefined = undefined;
-
-	// Get the relevant part of stdout containing the function analysis.
-	let regex = /-+(.*)1 file analyzed/gms;
-	let m = regex.exec(text);
-
-	if(null !== m){
-		let analyzedFunctionsText = m[1].trim().split('\n');
-
-		for(let functionLine of analyzedFunctionsText){
-			regex = /\s?(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+) (.*)@(\d+)-(\d+)@/gm;
-			m = regex.exec(functionLine);
-			if(null !== m) {
-				const linesOfCodeWithoutComments = parseInt(m[1], 10);
-				const cyclomaticComplexity = parseInt(m[2], 10);
-				const tokenCount = parseInt(m[3], 10);
-				const numOfParameters = parseInt(m[4], 10);
-				const functionName = m[6];
-				const lineNumberInEditor = parseInt(m[7], 10) - 1;
-
-				console.log(`linesOfCodeWithoutComments: ${linesOfCodeWithoutComments}\ncyclomaticComplexity: ${cyclomaticComplexity}\ntokenCount: ${tokenCount}\nnumOfParameters: ${numOfParameters}\nfunctionName: ${functionName}\nlineNumberInEditor: ${lineNumberInEditor}\n`);
-			}
-		}
-	}
-
-
-	return diagnosticCollection;
+function onDidCreate(uri: vscode.Uri[]){
+	console.log("created");
 }
 
 // this method is called when your extension is deactivated
---
+++ src/lizard.ts
@@ -0,0 +1,179 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+//---------------------------------------------------------------------------------------------------------------------
+
+enum State {
+  idle = 1,
+  inExecution,
+}
+
+let diagnosticCollection: vscode.DiagnosticCollection;
+
+let state: State = State.idle;
+let curLogFileUri: vscode.Uri;
+
+//---------------------------------------------------------------------------------------------------------------------
+
+export function createDiagnosticCollection(): vscode.DiagnosticCollection {
+	diagnosticCollection = vscode.languages.createDiagnosticCollection();
+	return diagnosticCollection;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+export function onDidEndTask(event: vscode.TaskEndEvent){
+	if (event.execution.task.definition.type === "LizardExecution") {
+		analyzeLizardLogFiles([curLogFileUri]);
+	}
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+export function lint()
+{
+	if((undefined !== vscode.window.activeTextEditor)
+	&& (undefined !== vscode.workspace.workspaceFolders)) {
+		const openedFilePath = vscode.window.activeTextEditor.document.uri.fsPath;
+		const openedFileName = path.parse(openedFilePath).name;
+		const logFolderUri = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, "log");
+		const logFileUri = vscode.Uri.joinPath(logFolderUri, openedFileName + ".log");
+
+		console.log(`openedFilePath ${openedFilePath}\nopenedFileName ${openedFileName}\nlogFolderUri ${logFolderUri.fsPath}\nlogFileUri ${logFileUri.fsPath}`);
+
+		// create lizard log folder if it does not exist
+		if(!fs.existsSync(logFolderUri.fsPath)) {
+			fs.mkdirSync(logFolderUri.fsPath);
+		}
+		// remove previously generated log file
+		if(fs.existsSync(logFileUri.fsPath)){
+			fs.rmSync(logFileUri.fsPath);
+			// fs.writeFileSync(logFileUri.fsPath, "");
+		}
+
+		// empty log file
+		fs.writeFileSync(logFileUri.fsPath, "");
+
+		let cmd = `lizard ${openedFilePath} >> ${logFileUri.fsPath}`;
+		let shellExecution = new vscode.ShellExecution(cmd);
+
+		state = State.inExecution;
+		curLogFileUri = logFileUri;
+		let task = new vscode.Task({type: "LizardExecution"},
+		vscode.workspace.workspaceFolders[0], "Execute lizard tool",
+		"lizard-linter",shellExecution);
+		vscode.tasks.executeTask(task);
+	}
+
+	vscode.window.showInformationMessage('Execute lint');
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+export function analyzeLizardLogFiles(lizardLogFilesUri: vscode.Uri[]){
+	console.log(`${lizardLogFilesUri}`);
+	let lizardDiagCol: Map<vscode.Uri, vscode.Diagnostic[] | undefined>;
+	for (let lizardLogFileUri of lizardLogFilesUri) {
+		if(fs.existsSync(lizardLogFileUri.fsPath)){
+			console.log("debug3");
+			vscode.workspace.openTextDocument(lizardLogFileUri).then((document) => {
+			console.log("debug4");
+			console.log(`${document.uri.fsPath}`);
+			let text = document.getText();
+			console.log(`text: ${text}`);
+			lizardDiagCol = analyzeLizardLogFile(text);
+
+			for (let lintDiagColKey of lizardDiagCol.keys()) {
+				diagnosticCollection.set(lintDiagColKey, lizardDiagCol.get(lintDiagColKey));
+			}
+		});
+		}
+	}
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+export function analyzeLizardLogFile(text: string): Map<vscode.Uri, vscode.Diagnostic[] | undefined>{
+	let diagnosticCollection = new Map<vscode.Uri, vscode.Diagnostic[] | undefined>();
+	let diagnostics: vscode.Diagnostic[] | undefined;
+	let previousFilePath: string | undefined = undefined;
+	let message: string;
+	let previousUri: vscode.Uri | undefined = undefined;
+
+	// Get the relevant part of stdout containing the function analysis.
+	let regex = /-+(.*)1 file analyzed/gms;
+	let m = regex.exec(text);
+
+	if(null !== m){
+		let analyzedFunctionsText = m[1].trim().split('\n');
+
+		for(let functionLine of analyzedFunctionsText){
+			regex = /\s?(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+) (.*)@(\d+)-(\d+)@(\S+)/gm;
+			m = regex.exec(functionLine);
+			if(null !== m) {
+				const linesOfCodeWithoutComments = parseInt(m[1], 10);
+				const cyclomaticComplexity = parseInt(m[2], 10);
+				const tokenCount = parseInt(m[3], 10);
+				const numOfParameters = parseInt(m[4], 10);
+				const functionName = m[6];
+				const lineStartNumberInEditor = parseInt(m[7], 10) - 1;
+				const lineEndNumberInEditor = parseInt(m[8], 10) - 1;
+				const filePath = m[9];
+				let fileUri = vscode.Uri.file(filePath);
+
+				if((undefined !== previousUri) && (previousUri.fsPath === fileUri.fsPath)) {
+					fileUri = previousUri;
+				}
+
+				// get or create new diag array
+				diagnostics = diagnosticCollection.get(fileUri);
+				if (undefined === diagnostics) {
+					diagnostics = [];
+				}
+
+				console.log(`linesOfCodeWithoutComments: ${linesOfCodeWithoutComments}\ncyclomaticComplexity: ${cyclomaticComplexity}\ntokenCount: ${tokenCount}\nnumOfParameters: ${numOfParameters}\nfunctionName: ${functionName}\nlineNumberInEditor: ${lineStartNumberInEditor}\nfileName: ${fileUri.fsPath}\n`);
+
+				const range: vscode.Range = new vscode.Range(lineStartNumberInEditor, 0, lineStartNumberInEditor, 1);
+				let threshold: number | undefined;
+
+				// cyclomatic complexity
+				const lizardLinterConfiguration = vscode.workspace.getConfiguration('thresholds');
+
+				threshold = lizardLinterConfiguration.get("cyclomaticComplexity");
+				if(undefined === threshold) {
+					threshold = 0;
+				}
+				if(cyclomaticComplexity > threshold) {
+
+					message = `Cyclomatic complexity of ${cyclomaticComplexity} higher then threshold (${threshold}) for function ${functionName}`;
+					diagnostics.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Warning));
+				}
+
+				// token count
+				threshold = lizardLinterConfiguration.get("tokenCount");
+				if(undefined === threshold) {
+					threshold = 0;
+				}
+				if(tokenCount > threshold) {
+					message = `Token count of ${tokenCount} higher then threshold (${threshold}) for function ${functionName}`;
+					diagnostics.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Warning));
+				}
+
+				// number of parameters
+				threshold = lizardLinterConfiguration.get("numOfParameters");
+				if(undefined === threshold) {
+					threshold = 0;
+				}
+				if(numOfParameters > threshold) {
+					message = `Number of parameters ${numOfParameters} higher then threshold (${threshold}) for function ${functionName}`;
+					diagnostics.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Warning));
+				}
+
+				previousUri = fileUri;
+				diagnosticCollection.set(fileUri, diagnostics);
+			}
+		}
+	}
+	return diagnosticCollection;
+}
